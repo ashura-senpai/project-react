3d86b5a89c2105b1133ce82e9ba1e845720f5d2a
ashuraa <91293924+ashura-senpai@users.noreply.github.com>
2024-06-14 03:48:21 +0000

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-    output: 'export',
+const nextConfig = {
+    output: "export",
 };
+
+export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-"use client"
 import React from 'react';
 import CardSection from "@/components/CardSection";
 import Header from "@/components/Header";
